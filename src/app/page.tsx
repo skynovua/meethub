6f1eafb5dd3d693cf6a88bdc11a4d2c370dd4a8b
Anton Yanovskyi <anton.yanovskyi@gmail.com>
2025-02-05 20:20:01 +0200

    fix: remove unused settings link from Dashboard component (#5)
    Co-authored-by: a.yanovskyi <anton.yanovskyi@sagax.software>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,6 @@ export default async function Dashboard() {
         <Button className="mb-4">
           <Link href={"/edit/new"}> Create Meetup</Link>
         </Button>
-        <Link href="/settings">asd</Link>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
           {meetups.map((meetup) => (
             <Card key={meetup.id}>
