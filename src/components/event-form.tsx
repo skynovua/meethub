bb065c8e1f6ede68eb91bc5eb5318c83e76dc764
a.yanovskyi <anton.yanovskyi@sagax.software>
2025-02-23 19:22:30 +0200

    feat: update VSCode settings for import organization and clean up event form imports

--- src/components/event-form.tsx
+++ src/components/event-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FormProvider, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Loader2 } from "lucide-react";
